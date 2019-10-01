21eb5480f4f62fe6c1ecb9c5f29c5a129d67bf80
Hayley Hollermann <hayleyhollermann@gmail.com>
2019-10-01 16:08:12 -0500

    created function for calculating bonus and pushing objects to new array

--- client.js
+++ client.js
@@ -40,4 +40,60 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+
+
+function bonusCalculator(employee) {
+  let totalPercentage = 0;
+  if (employee.reviewRating == 5) {
+    totalPercentage += .1;
+  }
+  else if (employee.reviewRating == 4) {
+    totalPercentage += .06;
+  }
+  else if (employee.reviewRating == 3) {
+    totalPercentage += .04;
+  }
+  if (employee.employeeNumber.length == 4) {
+    totalPercentage += .05;
+  }
+  if (employee.annualSalary > 65000) {
+    totalPercentage -= .01;
+  }
+  if (totalPercentage > .13) {
+    totalPercentage = .13;
+  }
+  else if (totalPercentage < 0){
+    totalPercentage = 0;
+  }
+  return totalPercentage;
+}
+
+//console.log(bonusCalculator(employees[2]));
+
+
+function newEmployeeObject (employee){
+    let percentBonus = bonusCalculator(employee);
+    let totalEmployeeBonus = percentBonus * employee.annualSalary;
+    let compensationPlusBonus = totalEmployeeBonus + Number(employee.annualSalary);
+    let newEmployee = {
+      name: employee.name,
+      bonusPercentage: percentBonus * 100,
+      totalCompensation: compensationPlusBonus,
+      totalBonus: totalEmployeeBonus,
+    }
+    return newEmployee;
+  }
+
+//console.log(newEmployeeObject(employees[1]));
+
+
+function bonusArray(array){
+  let newEmployeeArray = [];
+  for (employee of array){
+    newEmployeeArray.push(newEmployeeObject(employee));
+  }
+  return newEmployeeArray;
+}
+
+console.log(bonusArray(employees));
